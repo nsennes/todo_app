449d1b958355166894165939dd90fc9d758c049b
nsennes <panita.chavik@gmail.com>
2023-03-26 10:54:11 +0700

    finish

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile (){
+    return(
+        <div className="layout">
+            <img src="assests/nesnes (1).png" alt="profile"/>
+            <h1 className="name">Nes</h1>
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
